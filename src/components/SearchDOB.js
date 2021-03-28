364bdcde856abc6e97e07565c417d99666fbac09
desta mulu <destish21@yahoo.com>
2021-03-28 16:00:04 -0500

    Updated imported files SearchDOB.js

--- src/components/SearchDOB.js
+++ src/components/SearchDOB.js
@@ -7,17 +7,15 @@ const SearchDOB = () => {
 
   return (
     <div className="searchbox">
-       <div className="input-group">
-      <div className="input-group-prepend">
-        <span className="input-group-text" id="">
-          DOB
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="">
+            DOB
           </span>
-      </div>
+        </div>
+        <input type="date" className="form-control" onChange={e => context.handleSearchChange(e)} />
 
-      
-      <input type="date" className="form-control"   onChange={e => context.handleSearchChange(e)}/>
-      
-      <input type="date" className="form-control"   onChange={e => context.handleSearchChange(e)}/>
+        <input type="date" className="form-control" onChange={e => context.handleSearchChange(e)} />
       </div>
     </div>
   );
